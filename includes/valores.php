fbb2aac22f19c84fef980258735c26a5dcfe3ec3
facundo <facundo_petryszakotmail.com>
2026-04-24 21:48:27 -0300

    Mejora seccion valores

--- includes/valores.php
+++ includes/valores.php
@@ -4,7 +4,33 @@
             <div class="col">
                 <h2 class="text-center text-color-primary font-weight-semibold text-5 positive-ls-2 mb-0">El alma de Astrapia</h2>
                 <h3 class="text-center text-color-dark font-weight-bold text-transform-none text-10 mb-5">Nuestros Valores</h3>
-                <img src="img/valores/valores.png" class="img-fluid appear-animation animated fadeInUpShorter appear-animation-visible" data-appear-animation="fadeInUpShorter" alt="" style="animation-delay: 100ms;">
+                
+                <div class="valores-diagrama appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="100">
+                    <article class="valor-item valor-naturaleza">
+                        <div class="valor-icon"><i class="fas fa-hand-holding-heart"></i></div>
+                        <h4 class="valor-label">PASIÓN POR LA NATURALEZA</h4>
+                    </article>
+
+                    <article class="valor-item valor-privacidad">
+                        <div class="valor-icon"><i class="fas fa-shield-alt"></i></div>
+                        <h4 class="valor-label">PRIVACIDAD</h4>
+                    </article>
+
+                    <article class="valor-item valor-autenticidad">
+                        <div class="valor-icon"><i class="far fa-smile"></i></div>
+                        <h4 class="valor-label">AUTENTICIDAD</h4>
+                    </article>
+
+                    <article class="valor-item valor-lujo">
+                        <div class="valor-icon"><i class="far fa-gem"></i></div>
+                        <h4 class="valor-label">LUJO Y CONFORT</h4>
+                    </article>
+
+                    <article class="valor-item valor-sostenibilidad">
+                        <div class="valor-icon"><i class="fas fa-seedling"></i></div>
+                        <h4 class="valor-label">SOSTENIBILIDAD</h4>
+                    </article>
+                </div>
             </div>
         </div>
     </div>
